35a72b8dd9a817e9d277196deff1ab9ba6877846
Alex Artushin <aartushin@nucleoware.com>
2013-03-11 21:41:37 -0500

    Solution to the question. Added a couple of tests to the unit test class.

--- src/RewindableEntityIterator.class.php
+++ src/RewindableEntityIterator.class.php
@@ -2,7 +2,7 @@
 /**
  * Rewindable Entity Iterator
  *
- * @author You
+ * @author Alex Artushin <artushin@gmail.com>
  */
 
 namespace org\mizer;
@@ -10,4 +10,39 @@ namespace org\mizer;
 class RewindableEntityIterator extends EntityIterator
 {
 
+    /**
+     * @var array data storage
+     */
+    protected $data = array();
+
+
+    /**
+     * rewind back to index 0
+     */
+    public function rewind() {
+        $this->index = -1;
+        $this->next();
+        return;
+    }
+
+    /**
+     * Store the result if it is not yet in $data
+     * set next to be the result
+     * advance the pointer
+     *
+     * @return Object
+     */
+    public function next() {
+        if (isset($this->data[$this->index])){
+            $this->next = $this->data[$this->index];
+        }
+        else{
+            parent::next();
+            $this->data[$this->index] = $this->next;
+        }
+        $this->index++;
+        return $this->next;
+    }
+
 }
+
